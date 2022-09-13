4df1d4db88370f2ad1488aa9979f484698be6312
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-09-13 17:13:45 -0300

    req 08 test02

--- src/routers/salesRouter.js
+++ src/routers/salesRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const salesController = require('../controllers/salesController');
-// const productMiddle = require("../middlewares/ProductMiddle");
+const salesMiddle = require('../middlewares/salesMiddle');
 
 const router = express.Router();
 
 router.get('/', salesController.openProduct);
 
-router.get('/:id', salesController.openProduct);
+router.get('/:id', salesMiddle, salesController.openProduct);
 
 module.exports = router;
