0c1239876d279a227f337cf1644069d83af08906
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-09-13 19:38:24 -0300

    req 08 test 05

--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -2,13 +2,14 @@ const connection = require('./connection');
 
 const findByProductId = async (productId) => {
   const [result] = await connection.execute(
-    `SELECT sa.date date, pr.id productId, sp.quantity
+    `SELECT pr.id saleId, sa.date date, pr.id productId, sp.quantity
 FROM StoreManager.sales sa
-INNER JOIN StoreManager.products pr 
-ON sa.id = pr.id
 INNER JOIN StoreManager.sales_products sp
 ON sa.id = sp.sale_id
-WHERE sa.id = ?`,
+INNER JOIN StoreManager.products pr 
+ON sp.product_id = pr.id
+WHERE sa.id = ?
+ORDER BY pr.id`,
     [productId],
   );
   return result;
@@ -18,11 +19,11 @@ const findByAllId = async () => {
   const [result] = await connection.execute(
     `SELECT pr.id saleId, sa.date date, pr.id productId, sp.quantity
 FROM StoreManager.sales sa
-INNER JOIN StoreManager.products pr 
-ON sa.id = pr.id
 INNER JOIN StoreManager.sales_products sp
 ON sa.id = sp.sale_id
-ORDER BY sp.sale_id AND pr.id`,
+INNER JOIN StoreManager.products pr 
+ON sp.product_id = pr.id
+ORDER BY sp.sale_id, pr.id`,
   );
   return result;
 };
