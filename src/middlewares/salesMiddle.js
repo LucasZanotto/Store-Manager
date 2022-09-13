fe56252a1856b24b74bba5804897eafac097edd5
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-09-13 20:10:39 -0300

    req 08 test 08

--- src/middlewares/salesMiddle.js
+++ src/middlewares/salesMiddle.js
@@ -2,10 +2,10 @@ const findBy = require('../models');
 
 module.exports = async (req, res, next) => {
   const { id } = req.params;
-  const result = await findBy.productModel.findByAllId();
+  const result = await findBy.salesModel.findByAllId();
   console.log(result);
 
-  if (id > result.length || Number(id) === 0 || !id) {
+  if (id > (result.length - 1) || Number(id) === 0 || !id) {
     return res.status(404).json({ message: 'Sale not found' });
   }
 
